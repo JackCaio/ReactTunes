2c9b9467ad3ffd717038a01670e1c234dcd50110
JackCaio <caio.iack1@gmail.com>
2022-10-03 15:07:36 -0300

    Inicio projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </>
+    );
   }
 }
 
