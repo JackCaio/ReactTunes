b0350689e54510d5e6e5e3a98302530e757ba3c7
JackCaio <caio.iack1@gmail.com>
2022-10-03 16:53:38 -0300

    feat: Links de navegacao no Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
@@ -8,6 +9,27 @@ export default class Header extends Component {
       <div data-testid="header-component">
         <h1>Header</h1>
         <p data-testid="header-user-name">{name}</p>
+        <NavLink
+          to="/search"
+          activeClassName="active__nav"
+          data-testid="link-to-search"
+        >
+          Search
+        </NavLink>
+        <NavLink
+          to="/favorites"
+          activeClassName="active__nav"
+          data-testid="link-to-favorites"
+        >
+          Favorites
+        </NavLink>
+        <NavLink
+          to="/profile"
+          activeClassName="active__nav"
+          data-testid="link-to-profile"
+        >
+          Profile
+        </NavLink>
       </div>
     );
   }
