de22c2881d8f45db24321729ad2a9210d02dd30a
JackCaio <caio.iack1@gmail.com>
2022-10-03 16:36:39 -0300

    feat: Recupera dados de usuario para header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Header extends Component {
   render() {
+    const { name } = this.props;
     return (
-      <div data-testid="header-component" />
+      <div data-testid="header-component">
+        <h1>Header</h1>
+        <p data-testid="header-user-name">{name}</p>
+      </div>
     );
   }
 }
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+};
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      user: {},
+    };
+  }
+
+  handleLoading = async () => {
+    const user = await getUser();
+    this.setState({
+      loading: false,
+      user,
+    });
+  };
+
   render() {
+    const { loading, user } = this.state;
+    if (loading) this.handleLoading();
     return (
       <div data-testid="page-search">
-        <Header />
+        {loading ? <Loading /> : <Header { ...user } />}
       </div>
     );
   }
