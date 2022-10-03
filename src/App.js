cb6e3a614efd25a6e561be774dac358355ae7298
JackCaio <caio.iack1@gmail.com>
2022-10-03 15:10:40 -0300

    feat: Criadas rotas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
 import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import Search from './components/Search';
 
 class App extends React.Component {
   render() {
@@ -9,6 +15,12 @@ class App extends React.Component {
         <p>TrybeTunes</p>
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
         </Switch>
       </>
     );
