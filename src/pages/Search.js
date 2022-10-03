4f380293ea551a042263e2fe43348f2c348847ce
JackCaio <caio.iack1@gmail.com>
2022-10-03 17:50:37 -0300

    feat: Search bar

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,12 +3,14 @@ import Header from '../components/Header';
 import Loading from '../components/Loading';
 import { getUser } from '../services/userAPI';
 
+const SEARCH_LENGTH = 2;
 export default class Search extends Component {
   constructor() {
     super();
     this.state = {
       loading: true,
       user: {},
+      search: '',
     };
   }
 
@@ -20,12 +22,45 @@ export default class Search extends Component {
     });
   };
 
+  handleInput = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleButton = () => {
+    const { search } = this.state;
+    return search.length >= SEARCH_LENGTH;
+  };
+
   render() {
-    const { loading, user } = this.state;
+    const { loading, user, search } = this.state;
     if (loading) this.handleLoading();
     return (
       <div data-testid="page-search">
-        {loading ? <Loading /> : <Header { ...user } />}
+        {loading ? <Loading /> : (
+          <>
+            <Header { ...user } />
+            <label htmlFor="search">
+              <input
+                id="search"
+                name="search"
+                type="text"
+                data-testid="search-artist-input"
+                onChange={ this.handleInput }
+                value={ search }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ !this.handleButton() }
+            >
+              Pesquisar
+            </button>
+          </>
+        )}
       </div>
     );
   }
