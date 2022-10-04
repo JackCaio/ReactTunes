90fe5943b6cbe29e40855ac8ff561487fe3c2a9e
JackCaio <caio.iack1@gmail.com>
2022-10-04 17:06:25 -0300

    style: Mensagem para rota de erro

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found" />
+      <div data-testid="page-not-found">
+        <h1>404 - Page Not Found</h1>
+      </div>
     );
   }
 }
