74c1d4555888a0d11f18d137b7e265097046a6d5
JackCaio <caio.iack1@gmail.com>
2022-10-04 18:28:29 -0300

    feat: Pagina de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers = async () => {
+    const user = await getUser();
+    this.setState({
+      loading: false,
+      user,
+    });
+  };
+
   render() {
+    const { loading, user } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        {loading ? <Loading /> : (
+          <>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.description}</p>
+            <img
+              src={ user.image }
+              alt={ user.name }
+              data-testid="profile-image"
+            />
+            <Link to="/profile/edit">Editar perfil</Link>
+          </>
+        )}
+
       </div>
     );
   }
